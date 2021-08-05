d3512ce643af18e05c48176c0a63feadeaef60fc
Shweta-git-hub <shwetanere2014@gmail.com>
2021-08-06 02:20:03 +0530

    Initial commit

---
+++ js/client.js
@@ -0,0 +1,44 @@
+const socket = io('http://localhost:8000');
+const form = document.getElementById('send-container');
+const messageInput = document.getElementById('messageInp');
+const messageContainer= document.querySelector(".container");
+
+var audio = new Audio('ting1.mp3');
+
+const append = (message, position)=>{
+   const messageElement = document.createElement('div');
+   messageElement.innerText  = message;
+   messageElement.classList.add('message');
+   messageElement.classList.add(position);
+   messageContainer.append(messageElement);
+   if(position == 'left')
+   {
+    audio.play();
+   }
+  
+}
+
+
+form.addEventListener('submit', (e)=>{
+    e.preventDefault();
+    const message= messageInput.value;
+    append(`You: ${message}`,'right');
+    socket.emit('send', message);
+    messageInput.value = '';
+})
+
+
+const name = prompt("Enter your cool name to join");
+socket.emit('new-user-joined', name);
+
+socket.on('user-joined', name =>{
+     append(`${name} entered the chat`, 'left');
+})
+
+socket.on('receive', data =>{
+    append(`${data.name}: ${data.message} `, 'left');
+})
+
+socket.on('leave', name =>{
+    append(`${name} left the chat `, 'left');
+})
---
+++ nodeServer/index.js
@@ -0,0 +1,27 @@
+const io = require('socket.io')(8000, {
+    cors: {
+      origin: '*',
+    }
+  });
+const users = {};
+io.on('connection', socket =>{
+
+    socket.on('new-user-joined', name =>{
+       
+        // console.log("New User", name);
+        users[socket.id]= name;
+        socket.broadcast.emit('user-joined', name);   
+    });     
+
+    socket.on('send', message=>{ 
+        socket.broadcast.emit('receive',{message: message, name: users[socket.id]})
+    });
+
+    socket.on('disconnect', message=>{ 
+      socket.broadcast.emit('leave', users[socket.id]);
+      delete users[socket.id];
+  });
+
+
+
+}) 
